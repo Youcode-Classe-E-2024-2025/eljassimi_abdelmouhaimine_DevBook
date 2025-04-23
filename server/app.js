da53183cb204e4ac7cd16ca0099f7cb7c988338c
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-23 16:45:56 +0100

    loggin and register work with success

--- server/app.js
+++ server/app.js
@@ -1,24 +1,21 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require('cors');  // Une seule importation
 const connection = require('../config/db');
-
 const app = express();
 const PORT = 3000;
-
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 
 app.use(cors());
-app.use(bodyParser.json());
 
+app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/books', bookRoutes);
 
-app.get('/', (req,res)=>{
-    res.send('connected in app');
+app.get('/', (req, res) => {
+  res.send('connected in app');
 });
 
 app.listen(PORT, () => {
-    console.log('server running');
+  console.log(`Server running on port ${PORT}`);
 });
