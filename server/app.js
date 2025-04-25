19e90af917dae2637af1584bd73127af1b3089b6
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-25 10:02:31 +0100

    display book and create book works with success

--- server/app.js
+++ server/app.js
@@ -6,12 +6,17 @@ const PORT = 3000;
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(cors(
+  {
+    origin: 'http://127.0.0.1:5500',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true,
+  }
+));
 
 app.use(express.json());
+
 app.use('/auth', authRoutes);
 app.use('/books', bookRoutes);
 
